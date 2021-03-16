c3ed314f3c32c836bd5202e77c13a4c4626536fe
99zac <99zac@161.31.244.36>
2021-03-16 16:02:22 -0500

    1st commit

---
+++ TeamProject/client/MyClass.java
@@ -0,0 +1,17 @@
+package client;
+
+public class MyClass
+{
+
+  public static void main(String[] args)
+  {
+    //Print statements
+    System.out.println("Hello Joe");
+    
+    System.out.println("Another print statement 2");
+    
+    System.out.println("3rd Print statement");
+
+  }
+
+}
